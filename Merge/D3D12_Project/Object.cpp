77eaf5022735666c11eb7e461c3151ca38d7e7fd
Hong Dong Uk <hong991207@gmail.com>
2025-07-25 19:32:55 +0900

    플레이어 피격중 점프 안되게 수정

--- Merge/D3D12_Project/Object.cpp
+++ Merge/D3D12_Project/Object.cpp
@@ -241,12 +241,11 @@ void PlayerObject::ProcessInput(const GameTimer& gTimer)
         Idle();
     }
 
-    if ((keyState[VK_LBUTTON] & 0x88) == 0x80) { Attack(); }
+    if ((keyState[VK_LBUTTON] & 0x88) == 0x80)  Attack();
 
 
-    Gravity* gravity = GetComponent<Gravity>();
-    if ((keyState[VK_SPACE] & 0x88) == 0x80 && gravity) {
-        gravity->SetVerticalSpeed(20.0f);
+    if ((keyState[VK_SPACE] & 0x88) == 0x80) {
+        Jump();
     }
 }
 
@@ -277,6 +276,16 @@ void PlayerObject::Move(XMVECTOR dir, float speed,float deltaTime)
 
     float yaw = atan2f(XMVectorGetX(dir), XMVectorGetZ(dir)) * 180 / 3.141592f;
     transform->SetRotation({ 0.0f, yaw, 0.0f });
+}
+
+void PlayerObject::Jump()
+{
+    Gravity* gravity = GetComponent<Gravity>();
+    Animation* anim = GetComponent<Animation>();
+    if (!gravity) return;
+    if (anim->mCurrentFileName == "boy_hit.fbx") return;
+    if (anim->mCurrentFileName == "boy_dying_fix.fbx") return;
+    gravity->SetVerticalSpeed(20.0f);
 }
 
 void PlayerObject::Idle()
@@ -539,8 +548,8 @@ void TigerObject::Fire()
     mIsFired = true;
 
     Object* obj = new TigerAttackObject(m_scene, m_scene->AllocateId(), m_id);
-    obj->AddComponent(new Transform{ {0.0f, 6.0f, 16.0f} });
-    obj->AddComponent(new Collider{ {0.0f, 0.0f, 0.0f}, {2.0f, 6.0f, 6.0f} });
+    obj->AddComponent(new Transform{ {0.0f, 6.0f, 18.0f} });
+    obj->AddComponent(new Collider{ {0.0f, 0.0f, 0.0f}, {4.0f, 6.0f, 8.0f} });
     m_scene->AddObj(obj);
 }
 
